22562fa940e7dc30eeefe04b142296af060e89dd
k022g0093@g.neec.ac.jp <k022g0093@g.neec.ac.jp>
2023-10-23 10:16:25 +0900

    プロジェクト ファイルを追加します。

---
+++ Blossom Engine/GameScenes/GameScene.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "IScene.h"
+
+//#include "../components/camera/DebugCamera.h"
+#include "../Blossom Engine/components/camera/DebugCamera.h"
+
+class GameManager;
+class GameScene : public IScene
+{
+public:
+	// 初期化
+	void Initialize()override;
+
+	// 更新処理
+	void Update()override;
+
+	// 描画
+	void Draw()override;
+
+	void Finalize()override;
+private:
+	// カメラのポインタ
+	DebugCamera* camera_ = nullptr;
+
+	//// マップデータ
+	//Map* map_ = nullptr;
+
+	// デバッグ用の変数
+	int loadStageNum_ = 0;	// 読み込むステージの番号
+};
